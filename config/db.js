dccd3ebf3b44c3d8bf96a6547019db01f98d694f
bricktown88 <alexyoungcontact@gmail.com>
2020-01-26 19:24:01 -0600

    backend api complete

---
+++ config/db.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+// const connectDB = () => {
+//     mongoose.connect(db, {
+//         useNewUrlParser: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false
+//     }).then(() => console.log('MongoDB Connected'))
+//     .catch(err => {
+//         console.log(err.message);
+//         process.exit(1)
+//     })
+// };
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    console.log("MongoDB connected....");
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
 
-app.get('/', (req, res) => res.json({msg: 'Welcome to the ContactKeeper API'}))
+//connect database
+connectDB();
+
+//init middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the ContactKeeper API" })
+);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started on ${PORT}`))
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
 
 //define routes
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
